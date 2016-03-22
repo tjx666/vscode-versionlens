0198563e57337e3548aa79ef74eddbef3fd4bb19
pflannery <pflannery@users.noreply.github.com>
2016-03-22 05:16:18 +0000

    removes typedef files

--- src/extension.d.ts
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Peter Flannery. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-import {ExtensionContext} from 'vscode';
-export function activate(context: ExtensionContext);
