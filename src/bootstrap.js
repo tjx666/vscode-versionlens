178b02673d93c3fe18048d3409843ebab413b6e4
pflannery <pflannery@users.noreply.github.com>
2016-04-24 17:53:28 +0100

    replaces internal request module with external

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,15 +6,9 @@ import {register} from './common/di';
 const semver = require('semver');
 const bower = require('bower');
 const jsonParser = require('vscode-contrib-jsonc');
+const httpRequest = require('request-light');
 register('semver', semver);
 register('bower', bower);
-
-// load extension deps
-import {extensions} from 'vscode';
-const jsonExt = extensions.getExtension('vscode.json');
-const httpRequest = require(
-  jsonExt.extensionPath + '/server/out/utils/httpRequest'
-);
 register('jsonParser', jsonParser);
 register('httpRequest', httpRequest);
 
