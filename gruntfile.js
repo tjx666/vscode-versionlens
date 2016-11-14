ac091e7c4abe51fa93e7852c8c7e4e00ece57132
pflannery <pflannery@users.noreply.github.com>
2016-11-14 18:22:18 +0000

    minmizes build tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,1 @@
-module.exports = grunt => {
-  const pathUtil = require('path');
-  const configPath = pathUtil.join(process.cwd(), 'grunt');
-  const config = grunt.file.readYAML(pathUtil.join(configPath, 'config.yaml'));
-  config.env = process.env;
-
-  require('load-grunt-config')(grunt, {
-    jitGrunt: true,
-    configPath: configPath,
-    config: config
-  });
-
-};
+module.exports = grunt => require('load-grunt-config')(grunt, { jitGrunt: true })
