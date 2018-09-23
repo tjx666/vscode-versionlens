b3df46d334eeb9f17063fab9851f7669811543ae
Fernando Crespo <fernando82@gmail.com>
2018-09-23 01:03:43 -0300

    Added more tests

--- test/unit/providers/maven/mavenCodeLensProvider.tests.js
+++ test/unit/providers/maven/mavenCodeLensProvider.tests.js
@@ -15,13 +15,8 @@ const fixtureMap = new TestFixtureMap('./fixtures');
 
 let testContext = null
 
-function assertIsOlder(versionA, versionB) {
-  let result = compareVersions(versionA, versionB)
-  return (result < 0)
-}
-
-function assertIsNewer(versionA, versionB) {
-  return (compareVersions(versionA, versionB) > 0)
+function assertOrder(versionA, versionB) {
+  return (compareVersions(versionA, versionB) < 0)
 }
 
 function assertIsSame(versionA, versionB) {
@@ -53,50 +48,48 @@ export const MavenCodeLensProviderTests = {
     assert.equal(weightedQualifier('abc'), 'abc')
   },
   "Assert that first version": {
-    "Is older": () => {
-      assert.equal(assertIsOlder("0.99", "1"), true, '"0.99" < "1"')
-      assert.equal(assertIsOlder("1", "2"), true, '"1" < "2"')
-      assert.equal(assertIsOlder("1.5", "2"), true, '"1.5" < "2"')
-      assert.equal(assertIsOlder("1", "2.5"), true, '"1" < "2.5"')
-      assert.equal(assertIsOlder("1.0", "1.1"), true, '"1.0" < "1.1"')
-      assert.equal(assertIsOlder("1.1", "1.2"), true, '"1.1" < "1.2"')
-      assert.equal(assertIsOlder("1.0.0", "1.1"), true, '"1.0.0" < "1.1"')
-      assert.equal(assertIsOlder("1.0.1", "1.1"), true, '"1.0.1" < "1.1"')
-      assert.equal(assertIsOlder("1.1", "1.2.0"), true, '"1.1" < "1.2.0"')
+    "Is in order": () => {
+      assert.equal(assertOrder("0.99", "1"), true, '"0.99" < "1"')
+      assert.equal(assertOrder("1", "2"), true, '"1" < "2"')
+      assert.equal(assertOrder("1.5", "2"), true, '"1.5" < "2"')
+      assert.equal(assertOrder("1", "2.5"), true, '"1" < "2.5"')
+      assert.equal(assertOrder("1.0", "1.1"), true, '"1.0" < "1.1"')
+      assert.equal(assertOrder("1.1", "1.2"), true, '"1.1" < "1.2"')
+      assert.equal(assertOrder("1.0.0", "1.1"), true, '"1.0.0" < "1.1"')
+      assert.equal(assertOrder("1.0.1", "1.1"), true, '"1.0.1" < "1.1"')
+      assert.equal(assertOrder("1.1", "1.2.0"), true, '"1.1" < "1.2.0"')
+      assert.equal(assertOrder("3.8", "3.8.1"), true, '"3.8" < "3.8.1"')
 
-      assert.equal(assertIsOlder("1.0-alpha-1", "1.0"), true, '"1.0-alpha-1" < "1.0"')
-      assert.equal(assertIsOlder("1.0-alpha-1", "1.0-alpha-2"), true, '"1.0-alpha-1" < "1.0-alpha-2"')
-      assert.equal(assertIsOlder("1.0-alpha-1", "1.0-beta-1"), true, '"1.0-alpha-1" < "1.0-beta-1"')
+      assert.equal(assertOrder("1.0-alpha-1", "1.0"), true, '"1.0-alpha-1" < "1.0"')
+      assert.equal(assertOrder("1.0-alpha-1", "1.0-alpha-2"), true, '"1.0-alpha-1" < "1.0-alpha-2"')
+      assert.equal(assertOrder("1.0-alpha-1", "1.0-beta-1"), true, '"1.0-alpha-1" < "1.0-beta-1"')
 
-      assert.equal(assertIsOlder("1.0.1.20170215.211020-2", "1.0.7-SNAPSHOT"), true, '"1.0.1.20170215.211020-2" < "1.0.7-SNAPSHOT"')
+      assert.equal(assertOrder("1.0.1.20170215.211020-2", "1.0.7-SNAPSHOT"), true, '"1.0.1.20170215.211020-2" < "1.0.7-SNAPSHOT"')
 
-      assert.equal(assertIsOlder("1.0-1", "1.0.1"), true, '"1.0-1" < "1.0.1"')
+      assert.equal(assertOrder("1.0-beta-1", "1.0-SNAPSHOT"), true, '"1.0-beta-1" < "1.0-SNAPSHOT"')
+      assert.equal(assertOrder("1.0-SNAPSHOT", "1.0"), true, '"1.0-SNAPSHOT" < "1.0"')
+      assert.equal(assertOrder("1.0-alpha-1-SNAPSHOT", "1.0-alpha-1"), true, '"1.0-alpha-1-SNAPSHOT" < "1.0-alpha-1"')
 
-      assert.equal(assertIsOlder("1.0-beta-1", "1.0-SNAPSHOT"), true, '"1.0-beta-1" < "1.0-SNAPSHOT"')
-      assert.equal(assertIsOlder("1.0-SNAPSHOT", "1.0"), true, '"1.0-SNAPSHOT" < "1.0"')
-      assert.equal(assertIsOlder("1.0-alpha-1-SNAPSHOT", "1.0-alpha-1"), true, '"1.0-alpha-1-SNAPSHOT" < "1.0-alpha-1"')
+      assert.equal(assertOrder("1.0", "1.0-1"), true, '"1.0" < "1.0-1"')
+      assert.equal(assertOrder("1.0.0", "1.0-1"), true, '"1.0.0" < "1.0-1"')
+      assert.equal(assertOrder("1.0-1", "1.0.1"), true, '"1.0-1" < "1.0.1"')
+      assert.equal(assertOrder("1.0-1", "1.0-2"), true, '"1.0-1" < "1.0-2"')
 
-      assert.equal(assertIsOlder("1.0", "1.0-1"), true, '"1.0" < "1.0-1"')
-      assert.equal(assertIsOlder("1.0-1", "1.0-2"), true, '"1.0-1" < "1.0-2"')
-      assert.equal(assertIsOlder("1.0.0", "1.0-1"), true, '"1.0.0" < "1.0-1"')
+      assert.equal(assertOrder("1.0-alpha", "1.0"), true, '"1.0-alpha" < "1.0"')
+      assert.equal(assertOrder("2.0-1", "2.0.1"), true, '"2.0-1" < "2.0.1"')
+      assert.equal(assertOrder("2.0.1-klm", "2.0.1-lmn"), true, '"2.0.1-klm" < "2.0.1-lmn"')
+      assert.equal(assertOrder("2.0.1", "2.0.1-xyz"), true, '"2.0.1" < "2.0.1-xyz"')
 
-      assert.equal(assertIsOlder("2.0-1", "2.0.1"), true, '"2.0-1" < "2.0.1"')
-      assert.equal(assertIsOlder("2.0.1-klm", "2.0.1-lmn"), true, '"2.0.1-klm" < "2.0.1-lmn"')
-      assert.equal(assertIsOlder("2.0.1", "2.0.1-xyz"), true, '"2.0.1" < "2.0.1-xyz"')
+      assert.equal(assertOrder("2.0.1", "2.0.1-123"), true, '"2.0.1" < "2.0.1-123"')
+      assert.equal(assertOrder("2.0.1-xyz", "2.0.1-123"), true, '"2.0.1-xyz" < "2.0.1-123"')
 
-      assert.equal(assertIsOlder("2.0.1", "2.0.1-123"), true, '"2.0.1" < "2.0.1-123"')
-      assert.equal(assertIsOlder("2.0.1-xyz", "2.0.1-123"), true, '"2.0.1-xyz" < "2.0.1-123"')
 
-    },
-    "Is newer": () => {
-      assert.equal(assertIsNewer("1.1", "1.0"), true, '"1.1" > "1.0"')
-      assert.equal(assertIsNewer("1.1", "1.0.0"), true, '"1.1" > "1.0.0"')
-      assert.equal(assertIsNewer("1.0", "1.0-alpha"), true, '"1.0" > "1.0-alpha"')
-      assert.equal(assertIsNewer("3.8.1", "3.8"), true, '"3.8.1" > "3.8"')
+
     },
     "Is equal": () => {
       assert.equal(assertIsSame("1", "1.0"), true, '"1" = "1.0"')
       assert.equal(assertIsSame("2", "2.0.0"), true, '"2" = "2.0.0"')
+      assert.equal(assertIsSame("3.8.0", "3.8"), true, '"3.8.0" = "3.8"')
     }
   },
   "parseVersions": {
