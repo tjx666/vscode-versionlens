df70c84663cb51ccff2b4fd0c064784dda38c051
pflannery <pflannery@users.noreply.github.com>
2018-09-24 15:28:36 +0100

    Broken up versionUtil unit tests for easier management

---
+++ test/unit/common/versionUtils/pluckSemverVersions.tests.js
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Flannery. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { pluckSemverVersions } from 'common/versionUtils';
+
+const assert = require('assert');
+
+export default {
+
+  "removes non-semver versions": () => {
+    const testVersions = [
+      '2.3.4.12341',
+      '1.2.3.4444-test'
+    ];
+    const testResults = pluckSemverVersions(testVersions);
+    assert.equal(testResults.length, 0, "returned invalid semver versions");
+  },
+
+  "returns semver versions": () => {
+    const testVersions = [
+      '1.2.3',
+      '1.2.3-1',
+      '1.2.3-beta',
+      '1.2.3-beta+build',
+      '1.2.3+build'
+    ];
+
+    const testResults = pluckSemverVersions(testVersions);
+    assert.equal(testResults.length, testVersions.length, "returned invalid semver versions");
+  },
+
+}
