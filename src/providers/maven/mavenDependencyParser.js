b420e1d29c10aafe5e190e5db2170b08b7eb8726
fxcrespo <fxcrespo@al.sp.gov.br>
2018-10-01 16:32:09 -0300

    Support for maven properties in version and artifact

--- src/providers/maven/mavenDependencyParser.js
+++ src/providers/maven/mavenDependencyParser.js
@@ -1,5 +1,5 @@
 const xmldoc = require('xmldoc');
-const { Position, Range } = require('vscode');
+const { window } = require('vscode');
 
 export function findNodesInXmlContent(xmlContent, document, filterProperties) {
   const rootNode = new xmldoc.XmlDocument(document.getText());
@@ -53,28 +53,60 @@ export function extractDependencyNodes(rootNode, document, filterProperties) {
   return collector;
 }
 
+function extractPropertiesFromFile() {
+  let properties = []
+  if (window.activeTextEditor) {
+    let xmlCurrentPom = new xmldoc.XmlDocument(window.activeTextEditor.document.getText())
+    let propertiesCurrentPom = xmlCurrentPom.descendantWithPath("properties")
+    propertiesCurrentPom.eachChild(property => {
+      properties.push({
+        name: property.name,
+        val: property.val,
+        position: property.position
+      })
+    })
+  }
+  return properties;
+}
+
+
 function collectFromChildVersionTag(parentNode, includeRange, collector) {
   parentNode.eachChild(childNode => {
+    let versionNode;
     if (childNode.name !== "version")
       return;
     if (childNode.val.indexOf("$") >= 0) {
-      return
+      let properties = extractPropertiesFromFile()
+      let name = childNode.val.replace(/\$|\{|\}/ig, '')
+      versionNode = properties.filter(property => {
+        return property.name === name
+      })[0]
+    } else {
+      versionNode = childNode;
     }
     // TODO: Check if is a version variable like '${spring.version}' and evaluate to get the real version
 
     const replaceInfo = {
-      start: childNode.position,
-      end: childNode.position + childNode.val.length,
+      start: versionNode.position,
+      end: versionNode.position + versionNode.val.length,
     };
 
     let group = parentNode.childNamed("groupId").val
     let artifact = parentNode.childNamed("artifactId").val
 
+    let match = /\$\{(.*)\}/ig.exec(artifact);
+    if (match) {
+      let property = properties.filter(property => {
+        return property.name === match[1]
+      })[0]
+      artifact = artifact.replace(/\$\{.*\}/ig, property.val)
+    }
+
     collector.push({
       start: includeRange.start,
       end: includeRange.end,
       name: group + ":" + artifact,
-      value: childNode.val,
+      value: versionNode.val,
       replaceInfo
     });
   });
